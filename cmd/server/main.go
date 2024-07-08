ce939994ec3a88c7530ce374a751606763d5e442
Иван Хохлов <43066471+xoxloviwan@users.noreply.github.com>
2024-07-08 21:29:06 +0300

    replace net/http on gin

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"net/http"
-
+	"github.com/gin-gonic/gin"
 	"github.com/xoxloviwan/go-monitor/internal/api"
 )
 
 func main() {
 	handler := api.NewHandler()
-	err := http.ListenAndServe(":8080", handler)
+	ginHandler := gin.WrapH(handler)
+	r := gin.New()
+	r.Use(ginHandler)
+	err := r.Run(":8080")
 	if err != nil {
 		panic(err)
 	}
