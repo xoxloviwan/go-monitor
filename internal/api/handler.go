46c439594da527bad40041fc34dfd2879886121a
Иван Хохлов <43066471+xoxloviwan@users.noreply.github.com>
2024-07-09 09:32:37 +0300

    move func value() to handler.go

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -33,3 +33,22 @@ func update(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 	}
 }
+
+func value(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	metricType := req.PathValue("metricType")
+	metricName := req.PathValue("metricName")
+
+	v, ok := store.Storage.Get(metricType, metricName)
+
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+	} else {
+		w.Write([]byte(v))
+	}
+
+}
