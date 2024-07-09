6cda5c35c892e74c6e427300551fff510b404fa2
Иван Хохлов <43066471+xoxloviwan@users.noreply.github.com>
2024-07-09 09:46:09 +0300

    add tests for value()

--- internal/api/handler_test.go
+++ internal/api/handler_test.go
@@ -9,18 +9,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type want struct {
+	code        int
+	response    string
+	contentType string
+}
+
+type testcase []struct {
+	name   string
+	url    string
+	method string
+	want   want
+}
+
 func Test_update(t *testing.T) {
-	type want struct {
-		code        int
-		response    string
-		contentType string
-	}
-	tests := []struct {
-		name   string
-		url    string
-		method string
-		want   want
-	}{
+
+	tests := testcase{
 		{
 			name:   "service post 200 gauge",
 			url:    "/update/gauge/someMetric/23.4",
@@ -88,6 +92,70 @@ func Test_update(t *testing.T) {
 			}
 
 			update(w, req)
+
+			res := w.Result()
+			assert.Equal(t, tt.want.code, res.StatusCode)
+			defer res.Body.Close()
+		})
+	}
+}
+
+func Test_value(t *testing.T) {
+	tests := testcase{
+		{
+			name:   "service get 200 gauge",
+			url:    "/value/gauge/someMetric",
+			method: http.MethodGet,
+			want: want{
+				code:        200,
+				response:    "",
+				contentType: "plain/text",
+			},
+		},
+		{
+			name:   "service post 400 gauge",
+			url:    "/value/gauge/someMetric",
+			method: http.MethodPost,
+			want: want{
+				code:        400,
+				response:    "",
+				contentType: "plain/text",
+			},
+		},
+		{
+			name:   "service get 200 counter",
+			url:    "/value/counter/someMetric",
+			method: http.MethodGet,
+			want: want{
+				code:        200,
+				response:    "",
+				contentType: "plain/text",
+			},
+		},
+		{
+			name:   "service get 404",
+			url:    "/value/other/some",
+			method: http.MethodGet,
+			want: want{
+				code:        404,
+				response:    "",
+				contentType: "plain/text",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := httptest.NewRequest(tt.method, tt.url, nil)
+			w := httptest.NewRecorder()
+			urlSpl := strings.Split(tt.url, "/")
+			if len(urlSpl) > 2 {
+				req.SetPathValue("metricType", urlSpl[2])
+			}
+			if len(urlSpl) > 3 {
+				req.SetPathValue("metricName", urlSpl[3])
+			}
+
+			value(w, req)
 
 			res := w.Result()
 			assert.Equal(t, tt.want.code, res.StatusCode)
