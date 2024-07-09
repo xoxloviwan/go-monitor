91871b492e20d12f2aa9d59a78053a516868b181
Иван Хохлов <43066471+xoxloviwan@users.noreply.github.com>
2024-07-09 20:44:48 +0300

    add flag for server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/xoxloviwan/go-monitor/internal/api"
 )
 
 func main() {
+	adr := flag.String("a", "localhost:8080", "server adress")
+	flag.Parse()
+	if len(flag.Args()) > 0 {
+		fmt.Println("Too many arguments")
+		os.Exit(1)
+	}
 	r := api.SetupRouter()
 	r.Use(gin.Logger())
-	err := r.Run(":8080")
+	err := r.Run(*adr)
 	if err != nil {
 		panic(err)
 	}
