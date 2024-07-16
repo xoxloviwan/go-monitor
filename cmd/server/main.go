9becd9a0f6680debfe4479d60b18e77b73236fa4
Иван Хохлов <43066471+xoxloviwan@users.noreply.github.com>
2024-07-16 20:31:03 +0300

    remove extra

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,27 +1,13 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
-
 	"github.com/gin-gonic/gin"
 	"github.com/xoxloviwan/go-monitor/internal/api"
 	conf "github.com/xoxloviwan/go-monitor/internal/config_server"
 )
 
-const AddressDefault = "localhost:8080"
-
-type Config struct {
-	Address string `envDefault:"localhost:8080"`
-}
-
 func main() {
 	cfg := conf.InitConfig()
-	if len(flag.Args()) > 0 {
-		fmt.Println("Too many arguments")
-		os.Exit(1)
-	}
 	r := api.SetupRouter()
 	r.Use(gin.Logger())
 	err := r.Run(cfg.Address)
