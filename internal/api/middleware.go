72d2b1413e7fc68976ab78c15690c1f4230a6106
Иван Хохлов <43066471+xoxloviwan@users.noreply.github.com>
2024-07-23 17:16:47 +0300

    add custom logger based on slog

--- internal/api/middleware.go
+++ internal/api/middleware.go
@@ -1,16 +1,52 @@
 package api
 
 import (
-	"fmt"
+	"log/slog"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+type reqPars struct {
+	URI      string
+	method   string
+	duration time.Duration
+}
+
+type respPars struct {
+	code     int
+	bodySize int
+}
+
+type logParams struct {
+	reqPars
+	respPars
+}
+
+func (l *logParams) String() string {
+	return l.reqPars.method + " - " + strconv.Itoa(l.respPars.code) + " - " + strconv.Itoa(l.respPars.bodySize) + " - " + l.reqPars.duration.String() + " - " + l.reqPars.URI
+}
+
 func logger() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		//fmt.Printf("%v+\n", ctx)
-		fmt.Println("The URL: ", ctx.Request.URL.Path)
+		// Start timer
+		start := time.Now()
+		// Process request
 		ctx.Next()
+
+		pars := logParams{
+			reqPars: reqPars{
+				URI:      ctx.Request.URL.Path,
+				method:   ctx.Request.Method,
+				duration: time.Since(start),
+			},
+			respPars: respPars{
+				code:     ctx.Writer.Status(),
+				bodySize: ctx.Writer.Size(),
+			},
+		}
+
+		slog.Info(pars.String())
 	}
-	//return gin.LoggerWithWriter(gin.DefaultWriter, "[api] ")
 }
