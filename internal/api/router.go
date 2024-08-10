b2a7af24f3d76b486728610296096808b8a7d060
iwankho <43066471+xoxloviwan@users.noreply.github.com>
2024-08-10 15:15:55 +0300

    replace on gin context error

--- internal/api/router.go
+++ internal/api/router.go
@@ -26,7 +26,7 @@ func RunServer(cfg config.Config) error {
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 		if err := db.PingContext(ctx); err != nil {
-			log.Println("ping error:", err)
+			c.Error(err)
 			c.Status(http.StatusInternalServerError)
 			return
 		}
